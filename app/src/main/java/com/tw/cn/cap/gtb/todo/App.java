2d973a68c727935730c391b71f98450d8a58f5fa
handc <15289350682@163.com>
2022-06-07 18:09:13 +0800

    feat: show pure task name in list

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -1,8 +1,22 @@
 package com.tw.cn.cap.gtb.todo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
         throw new UnsupportedOperationException();
+    }
+
+    public List<String> run() {
+
+        try {
+            return Files.readAllLines(Path.of("C:/Users/huan/.todo/tasks"));
+        } catch (IOException e) {
+           throw  new ToDOCannotReadFileException();
+        }
     }
 }
