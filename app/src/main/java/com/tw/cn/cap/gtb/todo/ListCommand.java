667118d84b245a21909ed72c24c2da7241be9d8d
handc <15289350682@163.com>
2022-06-27 14:31:33 +0800

    refactor: extract ListCommand

---
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/ListCommand.java
@@ -0,0 +1,18 @@
+package com.tw.cn.cap.gtb.todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListCommand {
+    final TaskRepository taskRepository = new TaskRepository();
+
+
+    List<String> run() {
+        List<Task> tasks = taskRepository.loadTasks();
+        List<String> result = new ArrayList<>();
+        result.addAll(Section.tbd().format(tasks));
+        result.addAll(Section.completed().format(tasks));
+
+        return result;
+    }
+}
