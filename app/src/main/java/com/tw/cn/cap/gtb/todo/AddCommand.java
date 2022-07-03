7cbaadc6ca4324f280fad78f1e9b214e2bf1903f
handc <15289350682@163.com>
2022-07-03 22:21:13 +0800

    feat: add task with multiple words as name via AppTest

--- app/src/main/java/com/tw/cn/cap/gtb/todo/AddCommand.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/AddCommand.java
@@ -1,6 +1,7 @@
 package com.tw.cn.cap.gtb.todo;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AddCommand {
     private final String[] args;
@@ -10,12 +11,11 @@ public class AddCommand {
         this.args = args;
     }
 
-    public String[] getArgs() {
-        return args;
-    }
-
     List<String> execute() {
-        return taskRepository.create(new Task(0, getArgs()[1], false));
+        final var taskName = List.of(args).stream()
+                .skip(1)
+                .collect(Collectors.joining(" "));
+        return taskRepository.create(new Task(0, taskName, false));
     }
 
 }
