c7d5310de5fdf19a6737af462f55e93bbfa980cc
handc <15289350682@163.com>
2022-07-04 18:47:47 +0800

    refactor: convert TaskFactory static methods to instance method

--- app/src/main/java/com/tw/cn/cap/gtb/todo/TaskRepository.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/TaskRepository.java
@@ -9,11 +9,13 @@ import java.util.List;
 public class TaskRepository {
 
 
+    private final TaskMarshaller taskMarshaller = new TaskMarshaller();
+
     List<Task> loadTasks() {
         List<String> lines = readTaskLines();
         List<Task> tasks = new ArrayList<>();
         for (int i = 0; i < lines.size(); i++) {
-            tasks.add(TaskMarshaller.unmarshal(i + 1, lines.get(i)));
+            tasks.add(taskMarshaller.unmarshal(i + 1, lines.get(i)));
         }
         return tasks;
     }
@@ -28,7 +30,7 @@ public class TaskRepository {
 
     public void create(final Task task) {
         try (var bw = Files.newBufferedWriter(Constants.TASKS_FILE_PATH, StandardOpenOption.APPEND)) {
-            final String line = TaskMarshaller.marshal(task);
+            final String line = taskMarshaller.marshal(task);
             bw.write(line);
             bw.newLine();
         } catch (IOException e) {
--- app/src/test/java/com/tw/cn/cap/gtb/todo/TaskMarshallerTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/TaskMarshallerTest.java
@@ -1,20 +1,28 @@
 package com.tw.cn.cap.gtb.todo;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 class TaskMarshallerTest {
 
+    private TaskMarshaller taskMarshaller;
+
+    @BeforeEach
+    void setUp() {
+        taskMarshaller = new TaskMarshaller();
+    }
+
     @Test
     void should_parse_completed_property_for_task() {
-        boolean isCompleted = TaskMarshaller.unmarshal(1, "+ foo").isCompleted();
+        boolean isCompleted = taskMarshaller.unmarshal(1, "+ foo").isCompleted();
         Assertions.assertFalse(isCompleted);
     }
 
     @Test
     void should_support_name_with_multiple_white_spaces() {
-        Task task = TaskMarshaller.unmarshal(1, "+  foo  bar   ");
+        Task task = taskMarshaller.unmarshal(1, "+  foo  bar   ");
         String name = task.getName();
         Assertions.assertEquals(" foo  bar   ", name);
     }
