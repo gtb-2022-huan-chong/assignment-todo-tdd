94094faf62d8ba82ab6ff99bb42b924f6c162f0a
handc <15289350682@163.com>
2022-07-04 22:59:16 +0800

    test: use stub only remove existing tasks

--- app/src/main/java/com/tw/cn/cap/gtb/todo/RemoveCommand.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/RemoveCommand.java
@@ -1,6 +1,7 @@
 package com.tw.cn.cap.gtb.todo;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class RemoveCommand {
@@ -14,9 +15,14 @@ public class RemoveCommand {
 
 
     public List<String> execute() {
-        Stream.of(restArgs)
+
+        final var ids = Stream.of(restArgs)
                 .map(Integer::valueOf)
-                .forEach(this.taskRepository::delete);
+                .collect(Collectors.toList());
+
+        taskRepository.all().stream().
+                filter(task -> ids.contains(task.getId()))
+                .forEach(task -> taskRepository.delete(task.getId()));
         return List.of();
     }
 }
--- app/src/test/java/com/tw/cn/cap/gtb/todo/RemoveCommandTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/RemoveCommandTest.java
@@ -1,22 +1,49 @@
 package com.tw.cn.cap.gtb.todo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class RemoveCommandTest {
 
+    private TaskRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = mock(TaskRepository.class);
+        when(repository.all()).thenReturn(List.of(
+                new Task(1, "Task 01", false, false),
+                new Task(2, "Task 02", false, false),
+                new Task(3, "Task 03", false, false),
+                new Task(4, "Task 04", false, false)
+        ));
+    }
+
     @Test
     void should_remove_multiple_tasks() {
-        final var repository = mock(TaskRepository.class);
-
         RemoveCommand command = new RemoveCommand(new String[]{"1", "3"}, repository);
 
         command.execute();
 
         verify(repository).delete(1);
         verify(repository).delete(3);
+    }
+
+    @Test
+    void should_only_remove_existing_tasks() {
+
+        RemoveCommand command = new RemoveCommand(new String[]{"1", "3", "404"}, repository);
+
+        command.execute();
+
+        verify(repository).delete(1);
+        verify(repository).delete(3);
+        verify(repository, never()).delete(404);
     }
 }
